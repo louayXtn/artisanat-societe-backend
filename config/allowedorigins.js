1d6cef478c9223e46c8c31c5e70195787f50d3a6
louayXtn <louayawadh2ec@gmail.com>
2025-11-09 00:30:29 +0100

    enable cors for frontend domain

--- config/allowedorigins.js
+++ config/allowedorigins.js
@@ -1,2 +1,2 @@
-const allowedOrigins = ["http://localhost:3000","https://artisana-2.onrender.com"];
+const allowedOrigins = ["http://localhost:3000","https://artisana-2.onrender.com","https://artisan-societe.onrender.com"];
 module.exports = allowedOrigins;
